e66463e09c45bd4725666a6696f6d1aefaa2cd47
Streamtg <202310475@issu.edu.do>
2025-08-04 01:21:19 -0400

    Update __init__.py

--- WebStreamer/server/__init__.py
+++ WebStreamer/server/__init__.py
@@ -1,12 +1,15 @@
-# Taken from megadlbot_oss <https://github.com/eyaadh/megadlbot_oss/blob/master/mega/webserver/__init__.py>
-# Thanks to Eyaadh <https://github.com/eyaadh>
-# This file is a part of TG-FileStreamBot
-
+import aiohttp_jinja2
+import jinja2
 from aiohttp import web
-from .stream_routes import routes
-
+from WebStreamer.server import routes  # importa tus rutas
 
 def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
+    app = web.Application()
+    
+    # Configurar Jinja2 con la carpeta de templates (ajusta ruta si hace falta)
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('WebStreamer/template'))
+
+    # Registrar rutas
+    app.add_routes(routes)
+
+    return app
